7378cee098005e54d4c3e5780d8e1ddbfa5c115c
Dan Grebb <dan@dgrebb.com>
2023-12-24 16:34:39 -0500

    style: add token plaintext notice to settings disclaimer section (#18)

--- src/settings/sections/DisclaimerSection.ts
+++ src/settings/sections/DisclaimerSection.ts
@@ -10,10 +10,20 @@ export class DisclaimerSection extends BaseSection {
 	protected onload(): void {
 		renderMarkdown(
 			this.app,
+			`> [!caution] Caution\n> ${this.cautionText}`,
+			this.containerEl.createDiv(styles.container),
+			this.plugin,
+		);
+		renderMarkdown(
+			this.app,
 			`> [!info] Note\n> ${this.disclaimerText}`,
 			this.containerEl.createDiv(styles.container),
 			this.plugin,
 		);
+	}
+
+	private get cautionText(): string {
+		return 'Please take caution when syncing this vault. GitHub token is stored in plaintext in `./.obsidian/plugins/github-embeds/data.json`.';
 	}
 
 	private get disclaimerText(): string {
